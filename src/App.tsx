77a5ffd686d3bb9fabe82a17534103e2202d2b28
rontendo-jp <ronalds@gmail.com>
2026-02-13 15:16:45 +0900

    add Google Analytics tracking component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './Component';
 import { Home } from './pages/Home';
@@ -7,6 +7,43 @@ import { Company } from './pages/Company';
 import { Contact } from './pages/Contact';
 import { PropertySapporo } from './pages/PropertySapporo';
 import { LanguageProvider, useLanguage } from './context/LanguageContext';
+
+// Analytics tracking component
+function GoogleAnalytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Inject gtag script if it doesn't exist
+    if (!document.getElementById('gtag-script')) {
+      const script = document.createElement('script');
+      script.id = 'gtag-script';
+      script.async = true;
+      script.src = 'https://www.googletagmanager.com/gtag/js?id=G-63MSEQ98JN';
+      document.head.appendChild(script);
+
+      const configScript = document.createElement('script');
+      configScript.id = 'gtag-config';
+      configScript.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-63MSEQ98JN');
+      `;
+      document.head.appendChild(configScript);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Track page view on route change
+    if (window.gtag) {
+      window.gtag('config', 'G-63MSEQ98JN', {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
 
 // ScrollToTop component to reset scroll position on route change
 function ScrollToTop() {
@@ -33,6 +70,7 @@ export default function App() {
   return (
     <LanguageProvider>
       <HashRouter>
+        <GoogleAnalytics />
         <ScrollToTop />
         <Layout>
           <Routes>
